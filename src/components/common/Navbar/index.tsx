97cbd3d20b4fce01f9990545155f95253009e6dc
florin977 <florin.docheru@yahoo.com>
2026-06-06 20:26:33 +0300

    Navbar now uses user's initials and a menu

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,14 +1,39 @@
-import { AppBar, Badge, Box, Button, IconButton, Toolbar, Typography } from "@mui/material"
+import { AppBar, Avatar, Badge, Box, Button, IconButton, Toolbar, Typography, Menu } from "@mui/material"
 import { Link, NavLink } from "react-router-dom"
 import "./NavBar.css"
 import logo from "../../../assets/logo.png"
 import { useAuth } from "../../../contexts/AuthContext/AuthContext"
 import { useCart } from "../../../contexts/CartContext/cart-context"
 import { ShoppingCart } from "@mui/icons-material"
+import { useState } from "react"
 
 function NavBar() {
-    const { user, logout } = useAuth()
+    const { user, logout, logoutAll } = useAuth()
     const { cart, openCart } = useCart()
+    const [avatarElement, setAvatarElement] = useState<HTMLElement | null>(null)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    function handleAvatarClick(event: React.MouseEvent<HTMLButtonElement>) {
+        if (!isMenuOpen) {
+            setIsMenuOpen(true)
+            setAvatarElement(event.currentTarget)
+        } else {
+            setIsMenuOpen(false)
+            setAvatarElement(null)
+        }
+    }
+
+    function handleLogout() {
+        setAvatarElement(null)
+        setIsMenuOpen(false)
+        logout()
+    }
+
+    function handleLogoutAll() {
+        setAvatarElement(null)
+        setIsMenuOpen(false)
+        logoutAll()
+    }
 
     return (
         <AppBar className="main-appbar" position="static">
@@ -51,20 +76,35 @@ function NavBar() {
                         </Button>
                     ) : (
                         <>
-                            <Button className="navbar-btn-login" onClick={logout} variant="text">
-                                Logout
-                            </Button>
-                            <Typography variant="body2" className="">
-                                {user?.username}
-                            </Typography>
+                            {user?.role === "Customer" && (
+                                <IconButton color="inherit" onClick={openCart} sx={{ ml: 1 }}>
+                                    <Badge badgeContent={cart?.itemCount ?? 0} color="primary">
+                                        <ShoppingCart />
+                                    </Badge>
+                                </IconButton>
+                            )}
+                            <IconButton sx={{ p: 0, ml: 1, mr: 1 }} onClick={handleAvatarClick}>
+                                <Avatar>{user?.username[0] + user?.username[1]}</Avatar>
+                            </IconButton>
+                            <Menu
+                                anchorEl={avatarElement}
+                                open={isMenuOpen}
+                                onClose={() => setIsMenuOpen(false)}
+                                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                                transformOrigin={{ vertical: "top", horizontal: "center" }}>
+                                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", p: 1 }}>
+                                    <Typography variant="body1" sx={{ mb: 1 }}>
+                                        {user?.username}
+                                    </Typography>
+                                    <Button onClick={handleLogout} variant="contained" color="error" sx={{ mb: 1 }}>
+                                        Logout
+                                    </Button>
+                                    <Button onClick={handleLogoutAll} variant="contained" color="error" sx={{ mb: 1 }}>
+                                        Logout All Devices
+                                    </Button>
+                                </Box>
+                            </Menu>
                         </>
-                    )}
-                    {user?.role === "Customer" && (
-                        <IconButton color="inherit" onClick={openCart}>
-                            <Badge badgeContent={cart?.itemCount ?? 0} color="primary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
                     )}
                 </Box>
             </Toolbar>
